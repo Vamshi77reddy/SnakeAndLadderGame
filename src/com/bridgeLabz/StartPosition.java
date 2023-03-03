e6c18cb02ec4872d230ad943b90ae2357da2e9a3
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-03-03 13:35:33 +0530

    Added Dice Number

--- src/com/bridgeLabz/StartPosition.java
+++ src/com/bridgeLabz/StartPosition.java
@@ -1,10 +1,24 @@
 package com.bridgeLabz;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class StartPosition {
 public static void main(String []args) {
 	int position=0;
+	int d1=0;
 	System.out.println("Welcome to snake and ladder game");
 	System.out.println("the player is at Position :: 0");
 
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter first player1 name :");
+	String p1=sc.nextLine();
+	Random random=new Random();
+
+	d1=random.nextInt(6)+1;
+	System.out.println("The dice number is ::" +d1);
+
+
+	
 }
 }
