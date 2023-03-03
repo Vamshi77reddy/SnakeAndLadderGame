b6d36b86266aee807b10462807c76f6909701bba
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-03-03 13:05:19 +0530

    first commit

---
+++ src/com/bridgeLabz/StartPosition.java
@@ -0,0 +1,10 @@
+package com.bridgeLabz;
+
+public class StartPosition {
+public static void main(String []args) {
+	int position=0;
+	System.out.println("Welcome to snake and ladder game");
+	System.out.println("the player is at Position :: 0");
+
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Teju Reddy
+ *
+ */
+module snakeAndLadder {
+}
